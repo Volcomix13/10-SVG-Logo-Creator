fca6beebacfb67b66ab0d7f542a735daac24462f
Volcomix <m.santo13@hotmail.com>
2023-05-22 20:44:24 -0600

    added prompts to index.js

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const questions = [
+    {
+        type: "input",
+        message: " Type 3 letters of your choice",
+        name: "Initials"
+    },
+    {
+        type: "input",
+        message: "Please pick a text color",
+        name: "text-color"
+    },
+    {   
+        type: "list",
+        message: "Please pick a shape from the options below",
+        name: "shape",
+        choices: ["circle", "triangle", "square"]
+    },
+    {
+        type: "input",
+        message: "Please pick a color for your shape",
+        name: "shape-color"
+    },
+];
+
