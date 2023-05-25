84259c3b46dba760b991808d74a03a111a6b7b89
Volcomix <m.santo13@hotmail.com>
2023-05-24 22:31:05 -0600

    created shape classes and methods

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 //Required packages for application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const shapes = require("./lib/shapes");
+const {Circle, Rectangle, Triangle} = require("./lib/shapes");
 
 //Questions for user input
 const questions = [
     {
         type: "input",
         message: " Type 3 letters of your choice",
-        name: "Initials"
+        name: "initials"
     },
     {
         type: "input",
         message: "Please pick a text color",
-        name: "text-color"
+        name: "text_color"
     },
     {   
         type: "list",
@@ -24,17 +24,62 @@ const questions = [
     {
         type: "input",
         message: "Please pick a color for your shape",
-        name: "shape-color"
+        name: "shape_color"
     },
 ];
+
+class SVG {
+    constructor(){
+        this.shape = "";
+        this.text = "";
+    }
+
+render(){
+    return `<svg version="1.1"
+    width="300" height="200"
+    xmlns="http://www.w3.org/2000/svg"> ${this.shape} ${this.text}</svg>`
+}
+getText(userText, textColor){
+    this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${userText}</text>`
+  }
+  setShape(userShape){
+    this.shape = userShape.render();
+  }
+}
+
+
+
+//   <rect width="100%" height="100%" fill="red" />
+
+//   <circle cx="150" cy="100" r="80" fill="green" />
+
+//   <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+
+
+
 
 //Initiates app and creates svg fie
 function init(){
     inquirer
         .prompt(questions)
         .then((response)=>{
-            console.log("got it!")
-            fs.writeFile("./logo.svg", shapes({...response}), (err)=>
+            console.log(response);
+            let shape;
+            if (response.shape === "circle"){
+                shape = new Circle()
+            }else if(response.shape === "rectangle"){
+                shape = new Rectangle()
+            }else{
+                shape = new Triangle()
+            }
+
+            shape.getColor(response.shape_color)
+                console.log(shape)
+            const svg = new SVG()
+            svg.getText(response.initials, response.text_color)
+                console.log(svg)
+            fs.writeFile("./logo.svg", shape({...response}), (err)=>
             err ? console.log(err) : console.log("Generating Logo!"))
         })
         .catch((error)=>{
